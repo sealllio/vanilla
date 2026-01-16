f9b1a8d12845e2d5360e84a132e2866230952ac2
sealllio <254192841+sealllio@users.noreply.github.com>
2026-01-16 16:28:08 +1100

    Slightly less laggy gyro

--- gui/ui/non_sdl/iio_device.c
+++ gui/ui/non_sdl/iio_device.c
@@ -11,12 +11,22 @@
 
 struct IIO_Sensors init_iio_devices() {
 	struct iio_context* iio_ctx = iio_create_default_context();
-	struct iio_device* gyro = iio_context_find_device(iio_ctx, "(null)_gyro");
-	struct iio_device* accel = iio_context_find_device(iio_ctx, "(null)_accel");
+	struct iio_device* gyro_d = iio_context_find_device(iio_ctx, "(null)_gyro");
+	struct iio_device* accel_d = iio_context_find_device(iio_ctx, "(null)_accel");
 
-	struct IIO_Sensors sensors;
-	sensors.gyro = gyro;
-	sensors.accel = accel;
+	struct IIO_Sensors sensors = {
+		.modifier = {
+    		.axis = {2, 0, 1}, 
+    		.scale_g = {-0.9, 0.9, 0.9}, // 0.9 because 1 has issues with wii u calibration 
+    		.scale_a = {-0.9, 0.9, 0.9}
+		}
+	};
+
+	for (int i = 0; i <3; i++){
+		sensors.gyro[i] = iio_device_get_channel(gyro_d, i);
+		sensors.accel[i] = iio_device_get_channel(accel_d, i);
+	}
+	
 	return sensors;
 }
 
@@ -24,8 +34,8 @@ void* iio_thread_loop(void *v_sensors){
 	struct IIO_Sensors *sensors = (struct IIO_Sensors *)v_sensors;
 	int i = 10000;
 	while (i){
-		push_single_sensor_event(SDL_SENSOR_GYRO, sensors->gyro, 0.9);
-		push_single_sensor_event(SDL_SENSOR_ACCEL, sensors->accel, 0.9);
+		push_single_sensor_event(SDL_SENSOR_GYRO, sensors->gyro, sensors->modifier.axis, sensors->modifier.scale_g);
+		push_single_sensor_event(SDL_SENSOR_ACCEL, sensors->accel, sensors->modifier.axis, sensors->modifier.scale_a);
 		//sleep(1);
 		i--;
 	}	
@@ -49,44 +59,19 @@ void push_sensor_event(SDL_SensorType sensor, float data[3], uint64_t timestamp)
 // switch gyro and accel has 4 channels: 3 data, then timestamp
 // data attributes: raw, scale, scale, scale_available
 // current_timestamp_clock, mount_matrix, sampling_frequency, sampling_frequency_available
-void push_single_sensor_event(SDL_SensorType sensor, struct iio_device *dev, float modifier){
+void push_single_sensor_event(SDL_SensorType sensor, struct iio_channel** channels, int axis[], float modifier[]){
 	float data[3];
 	struct iio_channel* channel;
 	// data is le:S16/16>>0, so should be enough?
 	char value_string[10];
 	char scale_string[10];
 	for (int i=0; i<3; i++){
-		channel = iio_device_get_channel(dev, i);
+		channel = channels[i];
 		iio_channel_attr_read(channel, "raw", value_string, 10);
 		iio_channel_attr_read(channel, "scale", scale_string, 10);
-		if (i == 0) data[2] = -1* modifier * strtof(value_string, NULL) * strtof(scale_string, NULL);
-		if (i == 1) data[0] = modifier * strtof(value_string, NULL) * strtof(scale_string, NULL);
-		if (i == 2) data[1] = modifier * strtof(value_string, NULL) * strtof(scale_string, NULL);
-		//data[i] = modifier * strtof(value_string, NULL) * strtof(scale_string, NULL);
+		data[axis[i]] = modifier[i] * strtof(value_string, NULL) * strtof(scale_string, NULL);
 	}
 
-	/**
-	#define JC_IMU_MAX_ACCEL_MAG		32767
-#define JC_IMU_ACCEL_RES_PER_G		4096
-#define JC_IMU_ACCEL_FUZZ		10
-#define JC_IMU_ACCEL_FLAT		0
-
-#define JC_IMU_PREC_RANGE_SCALE	1000
-#define JC_IMU_MAX_GYRO_MAG		32767000 
-#define JC_IMU_GYRO_RES_PER_DPS		14247 
-#define JC_IMU_GYRO_FUZZ		10
-#define JC_IMU_GYRO_FLAT		0
-
-JC_IMU_PREC_RANGE_SCALE *
-				      (imu_data[i].gyro_x)),
-				     ctlr->gyro_cal.scale[0],
-				     ctlr->imu_cal_gyro_divisor[0]);
-
-					 (s32)imu_data[i].accel_x *
-			    ctlr->accel_cal.scale[0]) /
-			    ctlr->imu_cal_accel_divisor[0]
-
-*/
 	// channel = iio_device_get_channel(dev, 3);
 	// char scale_string[10];
 	// iio_channel_attr_read(channel, "I don't understand timestamp", scale_string, 10);
--- gui/ui/non_sdl/iio_device.h
+++ gui/ui/non_sdl/iio_device.h
@@ -4,13 +4,20 @@
 #include <SDL2/SDL.h>
 #include <iio.h>
 
+struct IMU_modifier {
+    int axis[3];  // which in-game axis a controller's axis is mapped to. e.g. axis[0] = 2 means controller's x = wiiu z
+    float scale_g[3]; // multiplies gyro value. For inverting and scaling.
+    float scale_a[3]; 
+};
+
 struct IIO_Sensors {
-    struct iio_device* gyro;
-    struct iio_device* accel;
+    struct iio_channel* gyro[3];
+    struct iio_channel* accel[3];
+    struct IMU_modifier modifier;
 };
 
 struct IIO_Sensors init_iio_devices();
 
 void push_sensor_event(SDL_SensorType sensor, float data[3], uint64_t timestamp);
-void push_single_sensor_event(SDL_SensorType sensor, struct iio_device *dev, float modifier);
+void push_single_sensor_event(SDL_SensorType sensor, struct iio_channel** channels, int axis[], float modifier[]);
 void* iio_thread_loop(void * v_sensors);
